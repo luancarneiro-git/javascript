18023c89ec642365d936bbfc0089d2e9fa35adeb
luancarneiro-git <luan.carneiro011@gmail.com>
2026-02-21 22:52:58 -0300

    update do material de estudos sobre manipulação de DOM

---
+++ aula09/Projeto aprendizado getElementBy/assets/scripts/script.js
@@ -0,0 +1,11 @@
+    let x1 = document.getElementsByTagName('p')[2]
+    x1.style.color = 'red'
+
+    let x2 = document.getElementById('l2')
+    x2.style.color = "blue"
+
+    let x3 = document.getElementsByClassName('l1')[0]
+    x3.style.color = 'green'
+
+    let x4 = document.querySelector('h5')
+    x4.style.color = 'yellow'
