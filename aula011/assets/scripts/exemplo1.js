534a9d71c4fb4db32c09caf78af2407445e495d4
luancarneiro-git <luan.carneiro011@gmail.com>
2026-03-04 19:36:45 -0300

    inicio dos estudos sobre condições, iniciando por if else

---
+++ aula011/assets/scripts/exemplo1.js
@@ -0,0 +1,5 @@
+let vel = 50.01
+console.log(`a velocidade do seu carro é de ${vel} KM/h`)
+if (vel > 50){ //condição simples
+    console.log('e você está a cima do limite de velocidade, MULTADO!')
+}
---
+++ aula011/assets/scripts/exemplo2.js
@@ -0,0 +1,7 @@
+let pais = 'Brasil'
+
+if(pais == 'Brasil'){
+    console.log('você é Brasileiro')
+} else {
+    console.log('você é Estrangeiro')
+}
