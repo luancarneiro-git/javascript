9df51d3c4d6e59e4727dc4e245647da1e1626c2c
luancarneiro-git <luan.carneiro011@gmail.com>
2026-03-28 01:57:59 -0300

    atualizando os estudos

---
+++ aula012/ex2/script.js
@@ -0,0 +1,8 @@
+let hora = 11
+if(hora >= 5 && hora <= 11){
+    console.log(`Bom dia! já são ${hora} horas da manhã!`)
+} else if(hora >= 12 && hora <= 17){
+    console.log(`Boa tarde, já são ${hora} horas da tarde!`)
+} else{
+    console.log(`Boa noite! Já são ${hora} horas da noite!`)
+}
---
+++ aula012/script.js
@@ -0,0 +1,11 @@
+let n1 = 67
+console.log(`você tem ${n1} anos de idade`)
+if(n1 < 16){
+    console.log('ainda não poderá votar')
+}   else if(n1 <= 17 || n1 >= 67){
+    console.log('e o voto é opicional')
+} 
+    else{
+    console.log('e o voto é obrigatório')
+}
+
