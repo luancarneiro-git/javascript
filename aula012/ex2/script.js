1fe300ce43fc8da8701cb59aabf972f87ed72a0d
luancarneiro-git <luan.carneiro011@gmail.com>
2026-03-29 01:16:10 -0300

    atualizando o código

--- aula012/ex2/script.js
+++
@@ -1,8 +0,0 @@
-let hora = 11
-if(hora >= 5 && hora <= 11){
-    console.log(`Bom dia! já são ${hora} horas da manhã!`)
-} else if(hora >= 12 && hora <= 17){
-    console.log(`Boa tarde, já são ${hora} horas da tarde!`)
-} else{
-    console.log(`Boa noite! Já são ${hora} horas da noite!`)
-}
